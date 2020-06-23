aea86f2cdaa14e79a62319b71d373da0cab781dd
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-23 15:09:57 -0700

    updating functionality for remove from favorites

--- public/javascript/remove-favorite.js
+++ public/javascript/remove-favorite.js
@@ -1,67 +1,15 @@
-// async function removeFavoriteHandler(event) {
-//     event.preventDefault();
-//     console.log(event);
-  
-
-  
-//     // if there is a comment -- preventing from users submitting empty comments 
-//     // if (recipe_id) {
-//     //     const response = await fetch('/savedrecipes', {
-//     //         method: 'POST',
-//     //         body: JSON.stringify({
-//     //           recipe_id,
-//     //         }),
-//     //         headers: {
-//     //           'Content-Type': 'application/json'
-//     //         }
-//     //       });
-        
-//     //       if (response.ok) {
-//     //           document.location.replace('/savedrecipes');
-//     //         } else {
-//     //           alert(response.statusText);
-//     //       }
-
-//     // }
-
-
-//     const id = window.location.toString().split('/')[
-//         window.location.toString().split('/').length - 1
-//       ];
-//     console.log("this is the id" + id)
-      
-//       const response = await fetch(`/savedrecipes/${id}`, {
-//         method: 'DELETE',
-//         body: JSON.stringify({
-//           id: id
-//         }),
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       });
-      
-//       if (response.ok) {
-//         document.location.replace('/savedrecipes');
-//       } else {
-//         alert(response.statusText);
-//       }
-
-// }
-  
-//   document.querySelector('.remove-favorite-form').addEventListener('submit', removeFavoriteHandler);
-
-  async function removeFavoriteFormHandler(event) {
+async function removeFavoriteFormHandler(event) {
     event.preventDefault();
 
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
       
-      const response = await fetch(`/api/savedrecipes/${id}`, {
+      const response = await fetch(`/savedrecipes/${id}`, {
         method: 'DELETE',
         body: JSON.stringify({
-        recipe_id: id
-        // id: id
+        // recipe_id: id
+        id: id
         }),
         headers: {
           'Content-Type': 'application/json'
