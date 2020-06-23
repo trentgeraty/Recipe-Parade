30d75436dc6b0a831bcacc4993b2a3b748fd0374
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-23 13:36:03 -0700

    updating edit recipe functionality

--- public/javascript/edit-recipe.js
+++ public/javascript/edit-recipe.js
@@ -1,38 +1,35 @@
 async function editFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
+  const title = document.querySelector('input[name="edit-title"]').value.trim();
+  const ingredients = document.querySelector('input[name="edit-ingredients"]').value.trim();
+  const directions = document.querySelector('input[name="edit-directions"]').value.trim();
+  console.log(title);
+  console.log(ingredients);
+  console.log(directions);
 
-    const recipeTitle = document.querySelector('input[name="edit-title"]').value.trim();
-    const recipeIngredients = document.querySelector('input[name="edit-ingredients"]').value.trim();
-    const recipeDirections = document.querySelector('input[name="edit-directions"]').value.trim();
-    console.log(recipeTitle);
-    console.log(recipeIngredients);
-    console.log(recipeDirections)
-
-
-      
-      const response = await fetch(`/api/recipes/${id}`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          recipe_id: id,
-          recipeTitle,
-          recipeIngredients,
-          recipeDirections
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      });
-      
-      if (response.ok) {
-        document.location.replace('/dashboard/');
-      } else {
-        alert(response.statusText);
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+    
+    const response = await fetch(`/api/recipes/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title,
+        ingredients,
+        directions
+      }),
+      headers: {
+        'Content-Type': 'application/json'
       }
+    });
+    
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
 
 }
 
-document.querySelector('#edit-recipe-form').addEventListener('submit', editFormHandler);
+document.querySelector('.edit-recipe-form').addEventListener('submit', editFormHandler);
