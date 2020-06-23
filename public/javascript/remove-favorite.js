a80a58327d50c43c63728e29afb28e01bc835f9d
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-23 14:52:41 -0700

    saved recipe latest updates

--- public/javascript/remove-favorite.js
+++ public/javascript/remove-favorite.js
@@ -1,39 +1,67 @@
-async function removeFavoriteHandler(event) {
-    event.preventDefault();
-    console.log(event);
+// async function removeFavoriteHandler(event) {
+//     event.preventDefault();
+//     console.log(event);
   
 
   
-    // if there is a comment -- preventing from users submitting empty comments 
-    // if (recipe_id) {
-    //     const response = await fetch('/savedrecipes', {
-    //         method: 'POST',
-    //         body: JSON.stringify({
-    //           recipe_id,
-    //         }),
-    //         headers: {
-    //           'Content-Type': 'application/json'
-    //         }
-    //       });
+//     // if there is a comment -- preventing from users submitting empty comments 
+//     // if (recipe_id) {
+//     //     const response = await fetch('/savedrecipes', {
+//     //         method: 'POST',
+//     //         body: JSON.stringify({
+//     //           recipe_id,
+//     //         }),
+//     //         headers: {
+//     //           'Content-Type': 'application/json'
+//     //         }
+//     //       });
         
-    //       if (response.ok) {
-    //           document.location.replace('/savedrecipes');
-    //         } else {
-    //           alert(response.statusText);
-    //       }
+//     //       if (response.ok) {
+//     //           document.location.replace('/savedrecipes');
+//     //         } else {
+//     //           alert(response.statusText);
+//     //       }
 
-    // }
+//     // }
 
+
+//     const id = window.location.toString().split('/')[
+//         window.location.toString().split('/').length - 1
+//       ];
+//     console.log("this is the id" + id)
+      
+//       const response = await fetch(`/savedrecipes/${id}`, {
+//         method: 'DELETE',
+//         body: JSON.stringify({
+//           id: id
+//         }),
+//         headers: {
+//           'Content-Type': 'application/json'
+//         }
+//       });
+      
+//       if (response.ok) {
+//         document.location.replace('/savedrecipes');
+//       } else {
+//         alert(response.statusText);
+//       }
+
+// }
+  
+//   document.querySelector('.remove-favorite-form').addEventListener('submit', removeFavoriteHandler);
+
+  async function removeFavoriteFormHandler(event) {
+    event.preventDefault();
 
     const id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
       ];
-    console.log("this is the id" + id)
       
-      const response = await fetch(`/savedrecipes/${id}`, {
+      const response = await fetch(`/api/savedrecipes/${id}`, {
         method: 'DELETE',
         body: JSON.stringify({
-          id: id
+        //   recipe_id: id
+        // id: id
         }),
         headers: {
           'Content-Type': 'application/json'
@@ -41,12 +69,11 @@ async function removeFavoriteHandler(event) {
       });
       
       if (response.ok) {
-        document.location.replace('/savedrecipes');
+        document.location.replace('/savedrecipes/');
       } else {
         alert(response.statusText);
       }
-
+      
 }
-  
-  document.querySelector('.remove-favorite-form').addEventListener('submit', removeFavoriteHandler);
 
+document.querySelector('.remove-favorite-form').addEventListener('submit', removeFavoriteFormHandler);
